9d50f14e99342493b324b647c9ef2a2f8ba870cb
hloriana <hloriana@amazon.com>
2022-06-28 15:52:50 -0400

    added pre-push hook

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Let's see if ptr-pudh.js command hooks are being executed with just
+          git push
         </p>
         <a
           className="App-link"
